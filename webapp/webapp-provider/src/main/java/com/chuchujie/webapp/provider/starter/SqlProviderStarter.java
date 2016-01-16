1e1ff3c69c2c5cf0fee5eaabaae1651794c7d053
yangxq <18329029859@163.com>
2016-01-16 22:53:58 +0800

    优化项目结构、删除无用pom、class

--- webapp/webapp-provider/src/main/java/com/chuchujie/webapp/provider/starter/SqlProviderStarter.java
+++ webapp/webapp-provider/src/main/java/com/chuchujie/webapp/provider/starter/SqlProviderStarter.java
@@ -1,24 +1,30 @@
 package com.chuchujie.webapp.provider.starter;
 
-import java.io.IOException;
-
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SqlProviderStarter {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
-		// context.addBeanFactoryPostProcessor(configurer);
-		context.setConfigLocations(new String[] { "DubboSpringAll.xml" });
-		context.refresh();
-
-		context.start();
-		try {
-			// 按任意键退出
-			System.in.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println("加载 dubboxSpring begin");
+		PropertyConfigurator.configure(SqlProviderStarter.class
+				.getClassLoader().getResource("log4jtcp.properties"));
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"DubboSpringAll.xml");
+		// context.refresh();
+		// context.start();
+		new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						sleep(500L);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start();
+		System.out.println("加载 dubboxSpring end");
 	}
 }
