0b06119c921c609e0c974c0a305a2257cba73eae
yangxq <18329029859@163.com>
2016-01-16 23:10:06 +0800

    删除webapp-provider

--- webapp-provider/src/main/java/com/chuchujie/webapp/provider/utils/MemcachedUtils.java
+++
@@ -1,9 +0,0 @@
-package com.chuchujie.webapp.provider.utils;
-
-import com.danga.MemCached.MemCachedClient;
-
-public class MemcachedUtils {
-	public static MemCachedClient getMemCachedClient(){
-		return SpringContextHolder.getBean("memcachedClient");
-	}
-}
